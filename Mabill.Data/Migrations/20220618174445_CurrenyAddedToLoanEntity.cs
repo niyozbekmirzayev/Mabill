5366ea5a1813aeb7dc80a017777290fd31144683
Niyozbek Mirzayev <niyozbekmirzayev2018@gmail.com>
2022-06-18 22:45:42 +0500

    Changes migrated, database updated

---
+++ Mabill.Data/Migrations/20220618174445_CurrenyAddedToLoanEntity.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Mabill.Data.Migrations
+{
+    public partial class CurrenyAddedToLoanEntity : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "CurrencyType",
+                table: "Loans",
+                type: "integer",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "CustomCurrencyType",
+                table: "Loans",
+                type: "text",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CurrencyType",
+                table: "Loans");
+
+            migrationBuilder.DropColumn(
+                name: "CustomCurrencyType",
+                table: "Loans");
+        }
+    }
+}
